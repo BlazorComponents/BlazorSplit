08c6e784a90dabadb2d3db33e65afd842875e357
Vladimir Samoilenko <wsamprof@gmail.com>
2018-10-12 16:37:03 +0200

    Add docs folder

---
+++ docs/_content/BlazorSplit/blazorSplit.js
@@ -0,0 +1,29 @@
+﻿var blazorSplit = {
+    splitRef: {},
+
+    init: function (interopHelper, id, elements, options, initOptions) {
+        if (initOptions.onDrag) {
+            options.onDrag = function() {
+                interopHelper.invokeMethodAsync("OnDrag");
+            };
+        }
+
+        if (initOptions.onDragStart) {
+            options.onDragStart = function(sizes) {
+                interopHelper.invokeMethodAsync("OnDragStart", sizes);
+            };
+        }
+
+        if (initOptions.onDragEnd) {
+            options.onDragEnd = function(sizes) {
+                interopHelper.invokeMethodAsync("OnDragEnd", sizes);
+            };
+        }
+
+        try {
+            blazorSplit.splitRef[id] = window.Split(elements, options);
+        } catch (e) {
+            debugger;
+        }
+    }
+};
